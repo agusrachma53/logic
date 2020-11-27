7e75f6fbc8d123346f89d4295bbb57011901e4da
Agus Rachman <agusrachman@helio.id>
2020-11-28 00:58:21 +0700

    bugfix SUM_AND_ORDER

--- SUM_AND_ORDER.js
+++ SUM_AND_ORDER.js
@@ -1,29 +1,36 @@
 
 
-    /*
+/*
 
-    NOTE
-    =============================
-    * Fungsi ini di peruntukan untuk menjumlahkan value dala array
-    * Value tersebut akan di jumlahkan keseluruhan dengan tidak mengikut sertakan 
-    * setiap value 
-    * exp : [ 1, 2, 3, 4, 5 ] = Jumlahkan [ 2, 3, 4, 5 ] tanpa mengikut
-    * sertakan angka 1, hal ini berlaku untuk semua value 
+NOTE
+=============================
+* Fungsi ini di peruntukan untuk menjumlahkan value dala array
+* Value tersebut akan di jumlahkan keseluruhan dengan tidak mengikut sertakan 
+* setiap value 
+* exp : [ 1, 2, 3, 4, 5 ] = Jumlahkan [ 2, 3, 4, 5 ] tanpa mengikut
+* sertakan angka 1, hal ini berlaku untuk semua value 
+* exp : [5, 5, 5, 5, 5] = 20 || karena perhitungan nya selalu menghilangkan 1 angka
 
-    */
+*/
 
-
-    let arr = [7,69,2,221,8974]
-    let arrRes = [];
-    for(let i = 0; i <= arr.length - 1; i++){
-        let sum = 0;
-         for(let j = 0; j <= arr.length - 1; j++){
-             if(arr[i] !== arr[j]){
+let arr = [5, 5, 5, 5, 5]
+let arrRes = [];
+for (let i = 0; i <= arr.length - 1; i++) {
+    let allEqual = arr => arr.every(v => v === arr[0])
+    let sum = 0;
+    for (let j = 0; j <= arr.length - 1; j++) {
+        if (allEqual(arr) === true) {
+            if (j < arr.length - 1) {
                 sum = sum + arr[j];
-             }
-         }
-         arrRes.push(sum);
+            }
+        } else {
+            if (arr[i] !== arr[j]) {
+                sum = sum + arr[j];
+            }
+        }
     }
-    let sort = arrRes.sort();
-    console.log(sort)
-    console.log(`${sort[0]} ${sort[sort.length - 1]}`)
+    arrRes.push(sum);
+}
+let sort = arrRes.sort();
+console.log(sort)
+console.log(`${sort[0]} ${sort[sort.length - 1]}`)
